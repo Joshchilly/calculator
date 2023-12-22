729b25a17b38d955c5d7043f5ea1cceceb743cf1
Josh Isaacson <65214680+Joshchilly@users.noreply.github.com>
2023-12-22 01:21:20 -0500

    Finish calculator
    - Changed some of display updating logic. Added function to update
      display.
    - Added minus button handling, decimal button handling, clear button
      handling, delete button handling, and surprise button handling.

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const calcCurrent = {
     secondNum: null,
     operator: null,
     operatorShowing: false,
-    dotButtonPressed: false,
+    decimalButtonPressed: false,
+    surpriseIndex: 0
 };
 
 const topScreen = document.querySelector('#top-screen');
@@ -15,26 +16,30 @@ const addBtn = document.querySelector('#add');
 const multiplyBtn = document.querySelector('#multiply');
 const divideBtn = document.querySelector('#divide');
 const subtractBtn = document.querySelector('#subtract');
-const dotBtn = document.querySelector('#dot');
+const decimalBtn = document.querySelector('#dot');
 const surpriseBtn = document.querySelector('#surprise');
-// Our number buttons are almost ordered in HTML file -> NodeList is almost ordered -> buttons array is almost ordered
-// !!!!! might not need these next two lines, and instead just const numBtns = document.querySelectorAll('.number') !!!!!
-const numBtns = Array.from(document.querySelectorAll('.number'));
-numBtns.unshift(numBtns.pop());
+const numBtns = document.querySelectorAll('.number');
 const operatorBtns = document.querySelectorAll('.operation');
+const buttons = document.querySelectorAll('button');
+const calcScreen = document.querySelector('#calc-screen');
+const surprise = ["Happiness can be found", "even in the darkest of times", "if only one remembers",
+    "to turn on the light"];
 
 numBtns.forEach(numBtn => numBtn.addEventListener('click', () => {
+    if (bottomScreen.textContent === "bruh") {
+        calcCurrent.secondNum = '';
+    }
     if (bottomScreen.textContent != null) {
-        if (bottomScreen.textContent.length === 12) {
+        if (bottomScreen.textContent.length >= 12 && (!calcCurrent.operatorShowing || calcCurrent.secondNum != null)) {
             return;
         }
     }
     if (calcCurrent.firstNum != null) {
-        if (calcCurrent.operator != null) {
+        if (calcCurrent.operator != null || calcCurrent.secondNum != null) {
             calcCurrent.secondNum == null ? calcCurrent.secondNum = numBtn.textContent :
                 calcCurrent.secondNum += numBtn.textContent;
-            topScreen.textContent = calcCurrent.firstNum + ' ' + calcCurrent.operator + ' ' + calcCurrent.secondNum;
-            bottomScreen.textContent = calcCurrent.firstNum + ' ' + calcCurrent.operator + ' ' + calcCurrent.secondNum;
+            calcCurrent.operatorShowing = false;
+            updateDisplaySecondNum();
             return;
         } else {
             calcCurrent.firstNum += numBtn.textContent;
@@ -43,60 +48,80 @@ numBtns.forEach(numBtn => numBtn.addEventListener('click', () => {
         calcCurrent.firstNum = numBtn.textContent;
     }
     calcCurrent.operatorShowing = false;
+    updateDisplayFirstNum();
+}));
+
+function updateDisplayFirstNum() {
     topScreen.textContent = calcCurrent.firstNum;
     bottomScreen.textContent = calcCurrent.firstNum;
-}));
+}
+
+function updateDisplaySecondNum() {
+    topScreen.textContent = calcCurrent.firstNum + ' ' + calcCurrent.operator + ' ' + calcCurrent.secondNum;
+    bottomScreen.textContent = calcCurrent.secondNum;
+}
 
 operatorBtns.forEach(opBtn => opBtn.addEventListener('click', () => {
+    if (calcCurrent.operator === '÷' && calcCurrent.secondNum == 0) {
+        bottomScreen.textContent = "bruh";
+        return;
+    }
+    if (opBtn.textContent != '=') {
+        calcCurrent.operatorShowing = true;
+    }
+    if (opBtn.textContent === '-') {
+        if (!handleMinus()) {
+            return;
+        }
+    }
     if (calcCurrent.secondNum == null) {
-        if ((calcCurrent.firstNum != null) && (calcCurrent.operator != '=')) {
+        if (calcCurrent.firstNum != null && opBtn.textContent != '=') {
             calcCurrent.operator = opBtn.textContent;
             topScreen.textContent = calcCurrent.firstNum + ' ' + calcCurrent.operator;
-            bottomScreen.textContent = topScreen.textContent;
-            calcCurrent.operatorShowing = true;
         }
     } else {
-        let result = calcExpression(Number(calcCurrent.firstNum), Number(calcCurrent.secondNum),
-            calcCurrent.operator);
-        if (result % 1 != 0) {
-            result = result.toFixed(5);
-        }
-        if (opBtn = '=') {
-            topScreen.textContent = calcCurrent.firstNum + ' ' + calcCurrent.operator + ' ' + calcCurrent.secondNum +
-                ' = ' + result;
-        } else {
-            topScreen.textContent = result + ' ' + calcCurrent.operator;
-            calcCurrent.operatorShowing = true;
-        }
-        calcCurrent.firstNum = result;
-        bottomScreen.textContent = result;
-        calcCurrent.secondNum = null;
+        calculateAndDisplay(opBtn);
     }
-    calcCurrent.dotButtonPressed = false;
+    calcCurrent.decimalButtonPressed = false;
 }));
 
-dotBtn.addEventListener('click', () => {
-    if (!calcCurrent.dotButtonPressed) {
-        if (calcCurrent.secondNum != null) {
-            calcCurrent.secondNum += '.';
-            topScreen.textContent = calcCurrent.firstNum + ' ' + calcCurrent.operator + ' ' + calcCurrent.secondNum;
-            bottomScreen.textContent = calcCurrent.secondNum;
-        } else {
-            if (calcCurrent.firstNum == null) {
-                calcCurrent.firstNum = '0.';
-                topScreen.textContent = calcCurrent.firstNum;
-                bottomScreen.textContent = calcCurrent.firstNum;
-            } else if (!calcCurrent.operatorShowing) {
-                calcCurrent.firstNum += '.';
-                topScreen.textContent = firstNum;
-                bottomScreen.textContent = firstNum;
-            }
-        }
-        if (!calcCurrent.operatorShowing) {
-            calcCurrent.dotButtonPressed = true;
-        }
+function handleMinus() {
+    if (calcCurrent.firstNum === '-' || calcCurrent.secondNum === '-') {
+        return 0;
     }
-});
+    else if (calcCurrent.firstNum == null) {
+        calcCurrent.firstNum = '-';
+        topScreen.textContent = '-';
+        bottomScreen.textContent = '-';
+        return 0;
+    } else if (calcCurrent.secondNum == null && calcCurrent.operator != null) {
+        calcCurrent.secondNum = '-';
+        topScreen.textContent = calcCurrent.firstNum + ' ' + calcCurrent.operator + ' ' + calcCurrent.secondNum;
+        bottomScreen.textContent = '-';
+        return 0;
+    }
+    return 1;
+} topScreen
+
+function calculateAndDisplay(opBtn) {
+    let result = calcExpression(Number(calcCurrent.firstNum), Number(calcCurrent.secondNum),
+        calcCurrent.operator);
+    result = result.toString().length < 12 ? result : result.toFixed(2).toString().length < 12 ? result.toFixed(2) :
+        result.toExponential(2).toString().length < 12 ? result.toExponential(2) : Infinity;
+    if (opBtn.textContent === '=') {
+        if (topScreen.textContent.search('=') === '-1') {
+            return;
+        }
+        topScreen.textContent = calcCurrent.firstNum + ' ' + calcCurrent.operator + ' ' + calcCurrent.secondNum +
+            ' = ' + result;
+    } else {
+        calcCurrent.operator = opBtn.textContent;
+        topScreen.textContent = result + ' ' + calcCurrent.operator;
+    }
+    calcCurrent.firstNum = result;
+    bottomScreen.textContent = result;
+    calcCurrent.secondNum = null;
+}
 
 function calcExpression(a, b, operator) {
     switch (operator) {
@@ -132,3 +157,73 @@ function divide(a, b) {
 function subtract(a, b) {
     return a - b;
 }
+
+decimalBtn.addEventListener('click', () => {
+    if (!calcCurrent.decimalButtonPressed) {
+        if (calcCurrent.secondNum != null || calcCurrent.operatorShowing) {
+            if (calcCurrent.secondNum != null && calcCurrent.secondNum != '-') {
+                calcCurrent.secondNum += '.';
+            }
+            else if (calcCurrent.secondNum === '-') {
+                calcCurrent.secondNum += '0.';
+            }
+            else {
+                calcCurrent.secondNum = '0.';
+            }
+            updateDisplaySecondNum();
+        } else {
+            if (calcCurrent.firstNum == null) {
+                calcCurrent.firstNum = '0.';
+            } else {
+                calcCurrent.firstNum += '.';
+            }
+            updateDisplayFirstNum();
+        }
+    }
+    calcCurrent.decimalButtonPressed = true;
+});
+
+clearBtn.addEventListener('click', () => {
+    calcCurrent.firstNum = null, calcCurrent.secondNum = null, calcCurrent.operator = null;
+    calcCurrent.operatorShowing = false, calcCurrent.decimalButtonPressed = false;
+    topScreen.textContent = '', bottomScreen.textContent = '';
+});
+
+deleteBtn.addEventListener('click', () => {
+    if (!calcCurrent.operatorShowing) {
+        if (calcCurrent.secondNum != null) {
+            calcCurrent.secondNum = Number(calcCurrent.secondNum.toString().slice(0, -1));
+            if (calcCurrent.secondNum === 0) {
+                calcCurrent.secondNum = '';
+            }
+            updateDisplaySecondNum();
+        } else if (calcCurrent.firstNum != null) {
+            calcCurrent.firstNum = Number(calcCurrent.firstNum.toString().slice(0, -1));
+            if (calcCurrent.firstNum === 0) {
+                calcCurrent.firstNum = '';
+            }
+            updateDisplayFirstNum();
+        }
+    }
+});
+
+surpriseBtn.addEventListener('click', () => {
+    if (calcCurrent.surpriseIndex >= 3) {
+        if (calcCurrent.surpriseIndex === 3) {
+            calcScreen.style.backgroundColor = "yellow";
+        } else {
+            calcCurrent.surpriseIndex = 0;
+            calcScreen.style.backgroundColor = "#4a4949";
+        }
+    }
+    topScreen.textContent = surprise[calcCurrent.surpriseIndex];
+    calcCurrent.surpriseIndex++;
+});
+
+buttons.forEach(btn => btn.addEventListener('click', (e) => {
+    if (e.target.textContent != '?' && calcCurrent.surpriseIndex != 0) {
+        clearBtn.click();
+        calcCurrent.surpriseIndex = 0;
+        calcScreen.style.backgroundColor = "#4a4949";
+    }
+}));
